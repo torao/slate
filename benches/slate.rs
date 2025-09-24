5627baa5979ca572157f9d86fccc1e44ad21d1d1
TAKAMI Torao <koiroha@gmail.com>
2025-09-24 09:47:47 +0900

    add benchmark target

--- benches/slate.rs
+++ benches/slate.rs
@@ -4,13 +4,13 @@ use std::path::MAIN_SEPARATOR;
 use std::{env::temp_dir as temporary_directory, path::PathBuf};
 
 use criterion::{Criterion, criterion_group, criterion_main};
-use slate::{BlockStorage, Slate};
+use slate::{BlockStorage, Prove, Slate};
 
 const ITER_SIZE: usize = 10 * 1024;
 
 fn append(c: &mut Criterion) {
   let path = temp_file("bench", ".db");
-  c.bench_function(&format!("append()×{ITER_SIZE}"), |b| {
+  c.bench_function(&format!("append() single call"), |b| {
     b.iter_batched(
       || {
         remove_file(&path).unwrap();
@@ -18,9 +18,7 @@ fn append(c: &mut Criterion) {
         Slate::new(storage).unwrap()
       },
       |mut slate| {
-        for i in 0..ITER_SIZE {
-          slate.append(&splitmix64(i as u64).to_le_bytes()).unwrap();
-        }
+        slate.append(&[0u8; 0]).unwrap();
       },
       criterion::BatchSize::PerIteration,
     );
@@ -28,26 +26,99 @@ fn append(c: &mut Criterion) {
   remove_file(&path).unwrap();
 }
 
-fn query(c: &mut Criterion) {
+fn get(c: &mut Criterion) {
   let path = temp_file("bench", ".db");
   let storage = BlockStorage::from_file(&path, false).unwrap();
   let mut slate = Slate::new(storage).unwrap();
-  for i in 0..ITER_SIZE {
-    slate.append(&splitmix64(i as u64).to_le_bytes()).unwrap();
+  for _ in 0..ITER_SIZE {
+    slate.append(&[0u8; 0]).unwrap();
   }
-  let mut r = splitmix64(0);
   let mut query = slate.snapshot().query().unwrap();
-  c.bench_function(&format!("query()×{ITER_SIZE}"), |b| {
+
+  // 事前にランダムなインデックス列を生成
+  let mut indices = Vec::with_capacity(ITER_SIZE);
+  let mut r = 0x12345678abcdefu64;
+  for _ in 0..ITER_SIZE {
+    let index = r % ITER_SIZE as u64 + 1;
+    indices.push(index);
+    r = splitmix64(r);
+  }
+
+  c.bench_function(&format!("get() single call"), |b| {
+    let mut index = 0;
     b.iter(|| {
-      let index = r % ITER_SIZE as u64 + 1;
-      query.get(index).unwrap().unwrap();
-      r = splitmix64(r);
+      let i = indices[index % ITER_SIZE];
+      query.get(i).unwrap().unwrap();
+      index += 1;
     });
   });
   remove_file(&path).unwrap();
 }
 
-criterion_group!(benches, append, query);
+fn prove(c: &mut Criterion) {
+  let files = 10;
+  let paths = (0..files).map(|i| temp_file(&format!("bench{i}"), ".db")).collect::<Vec<_>>();
+  let mut slates = paths
+    .iter()
+    .enumerate()
+    .map(|(num, path)| {
+      let storage = BlockStorage::from_file(path, false).unwrap();
+      let mut slate = Slate::new(storage).unwrap();
+      for i in 1..=ITER_SIZE {
+        if i == ITER_SIZE / files * num {
+          slate.append(&0u64.to_le_bytes()).unwrap();
+        } else {
+          slate.append(&i.to_le_bytes()).unwrap();
+        }
+      }
+      slate
+    })
+    .collect::<Vec<_>>();
+  let mut queries = slates.iter_mut().map(|slate| slate.snapshot().query().unwrap()).collect::<Vec<_>>();
+
+  // 事前にランダムなインデックス列を生成
+  let mut indices = Vec::with_capacity(ITER_SIZE);
+  let mut r = 0x12345678abcdefu64;
+  for _ in 0..ITER_SIZE {
+    let index = r as usize % files;
+    indices.push(index);
+    r = splitmix64(r);
+  }
+
+  c.bench_function(&format!("prove() single call"), |b| {
+    let mut index = 0;
+    b.iter(|| {
+      let i1 = indices[index % ITER_SIZE];
+      let i2 = indices[(index + ITER_SIZE / 2) % ITER_SIZE];
+
+      let mut base_i = ITER_SIZE as u64;
+      let _the_smallest_i_with_a_different_value = loop {
+        let auth1 = queries.get_mut(i1).unwrap().get_auth_path(base_i).unwrap().unwrap();
+        let auth2 = queries.get_mut(i2).unwrap().get_auth_path(auth1.leaf.i).unwrap().unwrap();
+        match auth1.prove(&auth2).unwrap() {
+          Prove::Identical => {
+            break None;
+          }
+          Prove::Divergent(diffs) => {
+            let (i, j) = diffs.iter().min_by(|a, b| a.0.cmp(&b.0)).copied().unwrap();
+            if j == 0 {
+              break Some(i);
+            }
+            base_i = i;
+          }
+        }
+      };
+
+      index += 1;
+    });
+  });
+
+  for path in paths {
+    remove_file(&path).unwrap();
+  }
+}
+
+criterion_group!(benches, append, get, prove);
 criterion_main!(benches);
 
 fn temp_file(prefix: &str, suffix: &str) -> PathBuf {
